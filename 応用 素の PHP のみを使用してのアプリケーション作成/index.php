999b96a92e392eb6d8c5cb6d17be316531d15a74
ymgc-kkk <67799475+ymgc-kkk@users.noreply.github.com>
2023-01-27 21:18:43 +0900

    finish curriculum

---
+++ 応用 素の PHP のみを使用してのアプリケーション作成/index.php
@@ -0,0 +1,104 @@
+<?php
+// 以下classでの共通化に置き換え
+// // 最初にDBへの接続を書きます
+// $dsn = 'mysql:dbname=test;host=127.0.0.1;port=3006;charset=utf8mb4';
+// $user= 'root';
+// $password = 'root';
+
+// try {
+//     $db  = new PDO($dsn, $user, $password);
+// } catch (PDOException $e) {
+//     echo "接続に失敗しました：" . $e->getMessage() . "\n";
+//     exit();
+// }
+
+// user接続処理
+require_once 'user.php';
+$user = new User();
+$users = $user->index();
+
+// 次に取得するための記述を行います。
+// $users = $db->query("SELECT * FROM users WHERE del_flg = false") ->fetchAll(PDO::FETCH_ASSOC);
+// del_flgは削除の有無を保持しています。デフォルトでは、0が入っているためfalseを指定することで削除されていないデータが取得対象になります。 
+
+// if (!empty($_GET['id'])) {
+//     $id = $_GET['id'];
+//     $sql = "UPDATE users SET del_flg = true WHERE id = :id AND del_flg = false";
+//     $stmt = $db->prepare($sql);
+//     $stmt->bindValue(':id', $id, PDO::PARAM_STR);
+//     $stmt->execute();
+//     header('Location: http://localhost:8080');
+// }
+
+$errorMessage = null;
+if (!empty($_GET['id'])) {
+    try {
+        $user->delete($_GET['id']);
+        header('Location: http://localhost:8080');
+    } catch (Exception $e) {
+        $errorMessage = $e->getMessage();
+    }
+}
+
+// var_dump($users);
+?>
+
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>TOPページ</title>
+</head>
+<body>
+    <div class="container w-auto inline-block px-8">
+        <?php if ($errorMessage): ?>
+            <div
+            class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+            role="alert">
+            <span class="block sm:inline"><?php echo $errorMessage ?></span>
+            </div>
+        <?php endif; ?>
+        <div class="mt-20 mb-10 flex justify-between">
+            <h1 class="text-base">登録者一覧</h1>
+            <button 
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+                <a href="/create.php">新規登録</a>
+            </button>
+        </div>
+        <div>
+            <table class="table-auto">
+                <thead>
+                    <tr>
+                        <th class="px-4 py-2">名前</th>
+                        <th class="px-4 py-2">住所</th>
+                        <th class="px-4 py-2">電話番号</th>
+                        <th class="px-4 py-2"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($users as $user): ?>
+                        <tr>
+                            <td class="border px-4 py-2"><?php echo $user['name'] ?></td>
+                            <td class="border px-4 py-2"><?php echo $user['address'] ?></td>
+                            <td class="border px-4 py-2"><?php echo $user['tel'] ?></td>
+                            <td class="border px-4 py-2">
+                                <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                                    <a href="<?php echo '/edit.php?id=' . $user['id'] ?>">編集</a>
+                                </button>
+                            </td>
+                            <td class="border px-4 py-2">
+                                <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                                    <a href="<?php echo '?id=' . $user['id'] ?>">削除</a>
+                                </button>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
