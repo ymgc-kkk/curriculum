1d975613cbc52e8a76f1261eb484dcf13ea8b26b
ymgc-kkk <67799475+ymgc-kkk@users.noreply.github.com>
2023-03-05 16:14:57 +0900

    Test finish

---
+++ Laravelの学習応用API化/testApp/tests/Feature/Api/TodoControllerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Todo;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TodoControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function setUp():void
+    {
+        parent::setUp();
+    }
+
+    public function Todoの新規作成()
+    {
+        $params = [
+            "title" => "テスト：タイトル",
+            "content" => "テスト：内容"
+        ];
+
+        $res = $this->postJson(route('api.todo.create'), $params);
+        $res->assertOk();
+        $todos = Todo::all();
+
+        $this->assertCount(1, $todos);
+
+        $todo = $todos->first();
+
+        $this->assertEquals($params['title'], $todo->title);
+        $this->assertEquals($params['content'], $todo->content);
+    }
+    
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
